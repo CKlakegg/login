b1012211aacc0cbcd5867cc6eb910475f1104391
Caroline <C.klakegg@gmail.com>
2023-10-25 15:45:07 +0200

    Initial Commit

---
+++ login/loginApp.swift
@@ -0,0 +1,17 @@
+//
+//  loginApp.swift
+//  login
+//
+//  Created by Caroline Klakegg on 25/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct loginApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
